738fdb5cefab1f2db25844c5158fbec077d55034
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-01-03 01:23:56 +0100

    Add align classes to removeClassName

--- src/js/utils/cleanClassName.js
+++ src/js/utils/cleanClassName.js
@@ -4,7 +4,11 @@ const cleanClassName = className => {
 	className = className.replace(/( )+/g, " ");
 	className = className.split(" ");
 	className = className.map(class_name =>
-		class_name.match(/^mbc-/) !== null ? null : class_name
+		class_name.match(
+			/(^mbc-|^alignleft|^aligncenter|^alignright|^alignwide|^alignfull)/
+		) !== null
+			? null
+			: class_name
 	);
 	className = compact(className);
 	className = className.join(" ");
