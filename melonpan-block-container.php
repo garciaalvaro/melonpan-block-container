b2d7819c7bd54dc7888ed7302f5c5010457364c5
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-05-26 13:05:45 +0200

    Update version number.

--- melonpan-block-container.php
+++ melonpan-block-container.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://wordpress.org/plugins/melonpan-block-container/
  * Description: Container block for Gutenberg editor.
  * Author: melonpan
- * Version: 1.0.1
+ * Version: 1.1.0
  * License: GPL3+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
@@ -15,7 +15,7 @@ namespace MELONPANBLOCKCONTAINER;
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 if ( ! defined( __NAMESPACE__ . '\PLUGIN_VERSION' ) ) {
-	define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.0.1' );
+	define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.1.0' );
 }
 if ( ! defined( __NAMESPACE__ . '\PLUGIN_NAME' ) ) {
 	define( __NAMESPACE__ . '\PLUGIN_NAME', 'melonpan-block-container' );
