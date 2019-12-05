33313909f4b72d0953196eb5e39e038c206724a9
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-12-05 11:04:00 +0900

    Use of import and path in webpack config files

--- webpack/build.config.babel.js
+++ webpack/build.config.babel.js
@@ -1,25 +1,26 @@
-const { name, version, description, homepage } = require("../package.json");
-const TerserJSPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { BannerPlugin, DefinePlugin } = require("webpack");
-const nib = require("nib");
+import { name, version, description, homepage } from "../package.json";
+import TerserJSPlugin from "terser-webpack-plugin";
+import OptimizeCSSAssetsPlugin from "optimize-css-assets-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BannerPlugin, DefinePlugin } from "webpack";
+import nib from "nib";
+import path from "path";
 
 export default {
 	entry: {
-		front: "./src/index-front.ts",
-		editor: "./src/index-editor.ts"
+		front: path.join(__dirname, "../src/index-front.ts"),
+		editor: path.join(__dirname, "../src/index-editor.ts")
 	},
 	output: {
-		path: __dirname + "/../build",
+		path: path.join(__dirname, "../build"),
 		filename: `${name}-[name].js`
 	},
 	resolve: {
 		alias: {
-			Components: __dirname + "/../src/Components",
-			block: __dirname + "/../src/block",
-			init: __dirname + "/../src/init",
-			utils: __dirname + "/../src/utils"
+			Components: path.join(__dirname, "../src/Components"),
+			block: path.join(__dirname, "../src/block"),
+			init: path.join(__dirname, "../src/init"),
+			utils: path.join(__dirname, "../src/utils")
 		}
 	},
 	externals: {
--- webpack/copy.config.babel.js
+++ webpack/copy.config.babel.js
@@ -1,39 +1,38 @@
-const { name } = require("../package.json");
-const CopyPlugin = require("copy-webpack-plugin");
+import { name } from "../package.json";
+import CopyPlugin from "copy-webpack-plugin";
+import path from "path";
 
-module.exports = [
-	{
-		entry: __dirname + "/copy.entry.js",
-		output: {
-			path: __dirname + "/../_release",
-			filename: "_temp.js"
-		},
-		plugins: [
-			new CopyPlugin([
-				{
-					from: "**/*",
-					ignore: [
-						".*",
-						".*/**",
-						"_extras/**",
-						"_release/**",
-						"_temp.js",
-						"assets-repo/**",
-						`build/${name}-front.js`,
-						"enzyme.config.js",
-						"jest*",
-						"node_modules/**",
-						"package.json",
-						"package-lock.json",
-						"pro/**",
-						"README.md",
-						"src/**",
-						"tsconfig.json",
-						"types.d.ts",
-						"webpack/**"
-					]
-				}
-			])
-		]
-	}
-];
+export default {
+	entry: path.join(__dirname, "copy.entry.js"),
+	output: {
+		path: path.join(__dirname, "../_release"),
+		filename: "_temp.js"
+	},
+	plugins: [
+		new CopyPlugin([
+			{
+				from: "**/*",
+				ignore: [
+					".*",
+					".*/**",
+					"_extras/**",
+					"_release/**",
+					"_temp.js",
+					"assets-repo/**",
+					`build/${name}-front.js`,
+					"enzyme.config.js",
+					"jest*",
+					"node_modules/**",
+					"package.json",
+					"package-lock.json",
+					"pro/**",
+					"README.md",
+					"src/**",
+					"tsconfig.json",
+					"types.d.ts",
+					"webpack/**"
+				]
+			}
+		])
+	]
+};
