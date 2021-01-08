7e73602aeaa1b39482869aa72b908fe5b8db2bcf
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-01-08 23:18:07 +0100

    Code formatted main PHP file

--- melonpan-block-container.php
+++ melonpan-block-container.php
@@ -12,24 +12,26 @@
 namespace MELONPANBLOCKCONTAINER;
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if (!defined("ABSPATH")) {
+	exit();
+}
 
-define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.3.1' );
-define( __NAMESPACE__ . '\PLUGIN_NAME', 'melonpan-block-container' );
-define( __NAMESPACE__ . '\BUILD_DIR', plugins_url( 'build/', __FILE__ ) );
+define(__NAMESPACE__ . "\PLUGIN_VERSION", "1.3.1");
+define(__NAMESPACE__ . "\PLUGIN_NAME", "melonpan-block-container");
+define(__NAMESPACE__ . "\BUILD_DIR", plugins_url("build/", __FILE__));
 
 /**
  * Enqueue the plugin style in the front page.
  *
  * @since 1.0.0
  */
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_front' );
-function enqueue_front() {
-
+add_action("wp_enqueue_scripts", __NAMESPACE__ . '\enqueue_front');
+function enqueue_front()
+{
 	wp_enqueue_style(
-		PLUGIN_NAME . '-front',
-		BUILD_DIR . PLUGIN_NAME . '-front.css',
-		array(),
+		PLUGIN_NAME . "-front",
+		BUILD_DIR . PLUGIN_NAME . "-front.css",
+		[],
 		PLUGIN_VERSION
 	);
 }
@@ -39,30 +41,34 @@ function enqueue_front() {
  *
  * @since 1.0.0
  */
-add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_editor', 900 );
-function enqueue_editor() {
-
+add_action(
+	"enqueue_block_editor_assets",
+	__NAMESPACE__ . '\enqueue_editor',
+	900
+);
+function enqueue_editor()
+{
 	wp_enqueue_style(
-		PLUGIN_NAME . '-editor',
-		BUILD_DIR . PLUGIN_NAME . '-editor.css',
-		array(),
+		PLUGIN_NAME . "-editor",
+		BUILD_DIR . PLUGIN_NAME . "-editor.css",
+		[],
 		PLUGIN_VERSION
 	);
 
 	wp_enqueue_script(
-		PLUGIN_NAME . '-editor',
-		BUILD_DIR . PLUGIN_NAME . '-editor.js',
-		array(
-			'lodash',
-			'wp-blocks',
-			'wp-block-editor',
-			'wp-components',
-			'wp-compose',
-			'wp-data',
-			'wp-element',
-			'wp-hooks',
-			'wp-i18n',
-		),
+		PLUGIN_NAME . "-editor",
+		BUILD_DIR . PLUGIN_NAME . "-editor.js",
+		[
+			"lodash",
+			"wp-blocks",
+			"wp-block-editor",
+			"wp-components",
+			"wp-compose",
+			"wp-data",
+			"wp-element",
+			"wp-hooks",
+			"wp-i18n",
+		],
 		PLUGIN_VERSION,
 		true // Enqueue in the footer.
 	);
